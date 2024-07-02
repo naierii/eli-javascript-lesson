b079b4f83e5ee6f3f7653a639a20a9f47d06f821
naierii <reianviloria@gmail.com>
2024-07-02 20:14:16 +0800

    query selector

---
+++ examples/HTML DOM methods/query selector/index.js
@@ -0,0 +1,4 @@
+// finds the first button element with class .special-button in the document
+const specialButtonDom = document.getQuerySelector('.special-button');
+
+console.log('specialButtonDom', specialButtonDom);
---
+++ exercises/DOM Manipulation Dynamic Query Selector/index.js
@@ -0,0 +1,13 @@
+// ============================================
+// Fill out Mahiiru's ID using Javascript
+// ============================================
+
+// FILL OUT NAME
+// Step 1: Find Mahiru's ID using document.getElementById
+
+
+// Step 2: using the found element, find the element with the class name 'card-name'
+
+
+// Step 3: using mahiru's data, replace the value inside using innerHTML
+
