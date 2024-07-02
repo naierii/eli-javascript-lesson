d57ef76343028aae5c7f19b90891bf550c684ef0
naierii <reianviloria@gmail.com>
2024-07-02 20:38:36 +0800

    edit query selector

--- examples/HTML DOM methods/query selector/index.js
+++ examples/HTML DOM methods/query selector/index.js
@@ -1,4 +1,8 @@
 // finds the first button element with class .special-button in the document
-const specialButtonDom = document.getQuerySelector('.special-button');
+const firstSpecialButton = document.querySelector('.special-button');
 
-console.log('specialButtonDom', specialButtonDom);
+const cardNumber2SpecialButton = document
+  .getElementById('cardNumber2')
+  .querySelector('.special-button');
+
+console.log('cardNumber2SpecialButton', cardNumber2SpecialButton);
