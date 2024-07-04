845e5507b76a3c98978fc1058bdba73c760bacc7
naierii <reianviloria@gmail.com>
2024-07-04 20:32:13 +0800

    example and exercise: querySelectorAll

---
+++ examples/HTML DOM methods/querySelectorAll/index.js
@@ -0,0 +1,6 @@
+// finds the first button element with class .special-button in the document
+const firstSpecialButton = document.querySelector('.special-button');
+const allSpecialButton = document.querySelectorAll('.special-button');
+
+console.log('firstSpecialButton', firstSpecialButton);
+console.log('allSpecialButton', allSpecialButton);
---
+++ exercises/DOM Manipulation Dynamic querySelectorAll/index.js
@@ -0,0 +1,11 @@
+// ============================================
+// Fill all card using the data from stage-girls-data.js
+// ============================================
+
+// FILL OUT CARD DETAILS 
+// Step 1: use querySelectorAll to get all the .card elements
+
+
+// Step 2: loop through the .card elements and fill out each elements' details
+// using the data from stage-girls-data.js
+
