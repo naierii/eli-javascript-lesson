8d78e2f3eea04f2bc2c246d3a351838acd048e5a
naierii <reianviloria@gmail.com>
2024-07-14 18:46:59 +0800

    exercise: dom and classes

---
+++ exercises/DOM and classes/index.js
@@ -0,0 +1,34 @@
+/**
+ * Use class
+ * Fill out the last two cards with Maya and Claudine's data
+ */
+
+class card{
+  constructor(stageGirlData){
+    this.stageGirlData = stageGirlData;
+    this.dom = dom;
+  }
+
+  fillCard(dom) {
+    /**
+     * create an algorithm inside this 
+     * method that uses the "dom" parameter
+     */
+  }
+}
+
+
+// const mayaData = howToGet
+// const claudineData = howToGet
+
+const mayaCard = new card(/*mayaData*/);
+const claudineData = new card(/*claudineData*/);
+
+// const thirdCardDom = document. nani
+// const fourthCardDom = document. nani
+
+/**
+ * Then use fillCard method you made from mayaCard and 
+ * claudineCard to fill the details in the 3rd dom
+ * and 4th dom
+ */
